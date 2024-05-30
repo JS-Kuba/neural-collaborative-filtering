21ce15c9feb7606234a9650fd95bedc55dac5ba9
r00648292 <Huawei2023#$>
2024-05-30 10:30:09 +0100

    Batchify the evaluation to handle large dataset.

--- src/train.py
+++ src/train.py
@@ -58,7 +58,7 @@ neumf_config = {'alias': 'neumf_factor8neg4',
                 'layers': [16, 64, 32, 16, 8],  # layers[0] is the concat of latent user vector & latent item vector
                 'l2_regularization': 0.0000001,
                 'weight_init_gaussian': True,
-                'use_cuda': True,
+                'use_cuda': False,
                 'device_id': 0,
                 'pretrain': False,
                 'pretrain_mf': 'checkpoints/{}'.format('gmf_factor8neg4_Epoch100_HR0.6391_NDCG0.2852.model'),
@@ -92,7 +92,7 @@ engine = NeuMFEngine(config)
 for epoch in range(config['num_epoch']):
     print('Epoch {} starts !'.format(epoch))
     print('-' * 80)
-    train_loader = sample_generator.instance_a_train_loader(config['num_negative'], config['batch_size'])
-    engine.train_an_epoch(train_loader, epoch_id=epoch)
+    # train_loader = sample_generator.instance_a_train_loader(config['num_negative'], config['batch_size'])
+    # engine.train_an_epoch(train_loader, epoch_id=epoch)
     hit_ratio, ndcg = engine.evaluate(evaluate_data, epoch_id=epoch)
     engine.save(config['alias'], epoch, hit_ratio, ndcg)
